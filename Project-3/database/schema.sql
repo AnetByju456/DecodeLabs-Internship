10a9c87ea4432a4204abf57bc83c187d6f89cecf
AnetByju456 <anetchirayath333@gmail.com>
2026-06-09 22:38:46 +0530

    Added cart_items schema

--- Project-3/database/schema.sql
+++ Project-3/database/schema.sql
@@ -76,3 +76,24 @@ CREATE TABLE carts (
     REFERENCES users(id)
     ON DELETE CASCADE
 );
+
+USE trademart;
+
+CREATE TABLE cart_items (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+
+    cart_id INT NOT NULL,
+
+    product_id INT NOT NULL,
+
+    quantity INT DEFAULT 1,
+
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+
+    FOREIGN KEY (cart_id)
+    REFERENCES carts(id)
+    ON DELETE CASCADE,
+
+    FOREIGN KEY (product_id)
+    REFERENCES products(id)
+);
